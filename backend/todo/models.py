41110a1a0bf2952050ee5548e794eb5eb11f0272
tharudc <93221751+tharudc@users.noreply.github.com>
2022-01-11 12:43:00 -0500

    first commit

---
+++ backend/todo/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class ToDo(models.Model):
+    '''
+    Model to represent a task or 'to-do'
+    '''
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
